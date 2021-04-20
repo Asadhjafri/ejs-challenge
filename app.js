1a2bcfe80d7348c47dc806cea40de0366e7df447
Asadhjafri <asadhjafri@gmail.com>
2021-04-20 11:46:25 -0400

    add for each loop to post blog title and content to home page after composing.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+let posts = [];
 
 app.set('view engine', 'ejs');
 
@@ -16,7 +17,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-  res.render("Home", {startingContent: homeStartingContent});
+  res.render("Home", {
+    startingContent: homeStartingContent,
+    posts: posts
+  });
+
 });
 
 app.get("/about", function(req, res){
@@ -32,8 +37,12 @@ app.get("/compose", function(req, res){
 });
 
 app.post("/compose", function(req, res){
-  newPost = req.body.postTitle
-  console.log(newPost)
+  const newPost = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  posts.push(newPost);
+  res.redirect("/");
 });
 
 
